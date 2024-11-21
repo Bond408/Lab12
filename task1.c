84379504a81b509ea7eafa7dbce6c11bce2f4d73
student <student@DESKTOP-1R0I5FG>
2024-11-21 18:17:14 +0300

    Добавьте файлы проекта.

---
+++ task1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <locale.h>
+
+int main(void) {
+	int *pi, i = 5;
+	char *pc, c = 'c';
+	double* pd, d = 1.03593574838834;
+	float *pf, f = 2.1f;
+	printf("%lu\n", sizeof(pi));
+	printf("%lu\n", sizeof(pf));
+	printf("%lu\n", sizeof(pc));
+	printf("%lu\n", sizeof(pd));
+
+
+
+
+
+}
